57e33be771bc2704d653cbf1ba9f14abf753906b
Aaron Brooks <abrooks@mastermnd.io>
2020-10-26 20:00:57 -0400

    Changed backend to S3

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "s3" {
+    bucket = "mastermnd-terraform-state"
+    key    = "pipelines/states"
+    region = "us-east-1"
+  }
 }
 
 # Configure the AWS Provider
